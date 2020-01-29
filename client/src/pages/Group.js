306fbef2591854c2d5aca76e96d75acd9b4c2d3a
Xander C <xander.canedo@gmail.com>
2020-01-28 20:10:47 -0700

    can delete groups successfully:

--- client/src/pages/Group.js
+++ client/src/pages/Group.js
@@ -19,6 +19,13 @@ class Group extends Component {
       .catch(err => console.log(err));
   };
 
+  deleteGroup = id => {
+    API.deleteGroup(id)
+      // .then(res => this.loadGroups())
+      .catch(err => console.log(err));
+      console.log("did I hit delete group? ID =" + id);
+  };
+
   render() {
     return (
         <>
@@ -53,13 +60,21 @@ class Group extends Component {
                   <ListItem key={group._id}>
                     <a href={"/groups/" + group._id}>
                       <strong>
-                        GroupID: {group.id}
+                        Group ID: {group._id}
                         <br/>
-                        {group.name}
+                        Group Name: {group.name}
                       </strong>
                     </a>
-                    <div className="delete-btn-placeholder">
-                      <p>Delete button goes here</p>
+                    <div className="flex items-center justify-center">
+                    <div className="m-3">
+                      <button className="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+                      onClick={() => this.deleteGroup(group._id)}>
+                        <span className="mr-2">Close</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="currentcolor" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
+                        </svg>
+                        </button>
+                      </div>
                     </div>
                   </ListItem>
                 ))}
